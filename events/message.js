d36728255c9725733091ab7d3131aa59829367d7
Arnav Gupta <Arnav000777gupta@gmail.com>
2022-02-03 23:25:05 +0530

    New Update

--- events/message.js
+++ events/message.js
@@ -1,33 +1,68 @@
 const client = require("../index.js");
-const { Webhook } = require("discord-webhook.js")
-
-client.on('messageCreate', async (message) => {
-  if (message.content.includes('Bablu_Bhai.gif')){
-    message.delete()
-  }
-})
+const { Webhook } = require("discord-webhook.js");
 
 client.on("messageCreate", async (message) => {
-	if (message.author.bot) return;
-	let msg = message.content
-	let emojis = msg.match(/(?<=:)([^:\s]+)(?=:)/g)
-	if (!emojis) return;
-	emojis.forEach(m => {
-		let emoji = client.emojis.cache.find(x => x.name === m)
-		if (!emoji) return;
-		let temp = emoji.toString()
-		if (new RegExp(temp, "g").test(msg)) msg = msg.replace(new RegExp(temp, "g"), emoji.toString())
-		else msg = msg.replace(new RegExp(":" + m + ":", "g"), emoji.toString());
-	})
-	if (msg === message.content) return;
-	let webhook = await message.channel.fetchWebhooks();
-	webhook = webhook.find(x => x.name === "NQN");
-	if (!webhook) {
-		webhook = await message.channel.createWebhook(`NQN`);
-	}
-	const hook = new Webhook(webhook.url)
-	hook.setUsername(message.author.username)
-	hook.setAvatar(message.author.avatarURL())
-	hook.send(msg)
-	message.delete().catch(err => { })
-})
+  if (message.content.includes("Bablu_Bhai.gif")) {
+    message.delete();
+  }
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  let msg = message.content;
+  let emojis = msg.match(/(?<=:)([^:\s]+)(?=:)/g);
+  if (!emojis) return;
+  emojis.forEach((m) => {
+    let emoji = client.emojis.cache.find((x) => x.name === m);
+    if (!emoji) return;
+    let temp = emoji.toString();
+    if (new RegExp(temp, "g").test(msg))
+      msg = msg.replace(new RegExp(temp, "g"), emoji.toString());
+    else msg = msg.replace(new RegExp(":" + m + ":", "g"), emoji.toString());
+  });
+  if (msg === message.content) return;
+  let webhook = await message.channel.fetchWebhooks();
+  webhook = webhook.find((x) => x.name === "NQN");
+  if (!webhook) {
+    webhook = await message.channel.createWebhook(`NQN`);
+  }
+  const hook = new Webhook(webhook.url);
+  message.delete().catch((err) => {});
+  hook.setUsername(message.author.username);
+  hook.setAvatar(message.author.avatarURL());
+  hook.send(msg);
+});
+
+const hooksend = async (msg, username, avatar) => {
+  let webhook = await message.channel.fetchWebhooks();
+  webhook = webhook.find((x) => x.name === "NQN");
+
+  if (!webhook) {
+    webhook = await message.channel.createWebhook(`NQN`);
+  }
+
+  const hook = new Webhook(webhook.url);
+  hook.setUsername(username);
+  hook.setAvatar(avatar);
+  hook.send(msg);
+};
+
+client.on("messageCreate", async (message) => {
+	if (message.author.id != "908554250945183744") return
+  if (message.content == "%spank") {
+    hooksend(
+      "<a:panties:937923765227114506>",
+      "Get spanked!",
+      "https://cdn.discordapp.com/emojis/937923765227114506.gif"
+    );
+    message.delete();
+  }
+	if (message.content == "%vibe") {
+    hooksend(
+      "<a:DJ:937922981823393813>",
+      "Vibing to music",
+      "https://cdn.discordapp.com/emojis/937922981823393813.gif"
+    );
+    message.delete();
+  }
+});
